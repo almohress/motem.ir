d983b2bba9cc4dc42ea01bc82accce7b43fe2bcd
alimohress <alimohress@gmail.com>
2021-08-08 14:10:48 +0430

    devel(contents): check if file is video

--- contents/models/multimedia.py
+++ contents/models/multimedia.py
@@ -1,9 +1,9 @@
+import uuid
+import os
+import magic
 from django.db import models
 from djrest_wrapper.interfaces import BaseModel
 
-
-import uuid
-import os
 
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
@@ -14,3 +14,9 @@ def get_file_path(instance, filename):
 class Multimedia(BaseModel):
     file = models.FileField(upload_to=get_file_path)
     last_modified = models.DateTimeField(auto_now_add=True)
+    is_video = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if magic.Magic(mime=True).from_buffer(self.file.file.file.read()).split('/')[0].lower() == 'video':
+            self.is_video = True
+        super().save(args, kwargs)
