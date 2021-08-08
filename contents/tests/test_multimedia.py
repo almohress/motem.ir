2180bb8b442adfe22d4b18fbdaa0ece7c39882e8
alimohress <alimohress@gmail.com>
2021-08-08 13:29:06 +0430

    tests(contents): correct test name

--- contents/tests/test_multimedia.py
+++ contents/tests/test_multimedia.py
@@ -5,10 +5,10 @@ from ..models import Multimedia
 
 class MultimediaTestCase(TestCase):
     def setUp(self):
-        with open('testfile.bin', mode='wb') as f:
+        with open('testfile.db', mode='wb') as f:
             f.write(b'\x10')
 
-    def test_fail(self):
-        with File(open('testfile.bin', mode='rb')) as f:
+    def test_create_multimedia(self):
+        with File(open('testfile.db', mode='rb')) as f:
             mm = Multimedia.objects.create(file=f)
             self.assertIsNotNone(mm)
