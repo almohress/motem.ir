cb1df78b98c67ccf147f9b90c00b8e15b8448a95
samurai <navazesh.hossein@gmail.com>
2021-08-11 13:39:20 -0400

    contents(urls):rename

--- contents/urls/__init__.py
+++ contents/urls/__init__.py
@@ -1,6 +1,5 @@
 
 from .category import urlpatterns as category_urls
-from .content import urlpatterns as content_urls
 
 
-urlpatterns = category_urls+content_urls
+urlpatterns = category_urls
--- contents/urls/category.py
+++ contents/urls/category.py
@@ -2,5 +2,5 @@ from rest_framework.routers import DefaultRouter
 from ..views import CategoryViewSet
 
 router = DefaultRouter()
-router.register(r'category', CategoryViewSet, basename='catagory')
+router.register(r'category', CategoryViewSet, basename='category')
 urlpatterns = router.urls
