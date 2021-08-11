b1979c62dfe1c0ed3d1637fd00c1bf2b19f4f126
samurai <navazesh.hossein@gmail.com>
2021-08-11 13:27:53 -0400

    contents(services): override category

--- contents/services/category.py
+++ contents/services/category.py
@@ -2,18 +2,5 @@ from djrest_wrapper.interfaces import BaseService
 
 
 class CategoryService(BaseService):
+    pass
 
-    def create_model(self, fields: dict):
-        pass
-
-    def list_model(self):
-        pass
-
-    def retrieve_model_by_id(self, id):
-        pass
-
-    def update_model_by_id(self, id, fields: dict):
-        pass
-
-    def delete_model_by_id(self, id):
-        pass
