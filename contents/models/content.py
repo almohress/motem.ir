6a8a5f4f408c62af5db0953fc900f068d412ad39
samurai <navazesh.hossein@gmail.com>
2021-08-13 13:07:43 -0400

    contents(model): fixed grammer`

--- contents/models/content.py
+++ contents/models/content.py
@@ -1,6 +1,6 @@
 from django.db import models
 from djrest_wrapper.interfaces import BaseModel
-from .catagory import Catagory
+from .category import Category
 from .multimedia import Multimedia
 
 
@@ -9,5 +9,5 @@ class Content(BaseModel):
     body = models.TextField()
     last_modified = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
-    catagory = models.ForeignKey(Catagory, on_delete=models.CASCADE)
+    catagory = models.ForeignKey(Category, on_delete=models.CASCADE)
     files = models.ManyToManyField(Multimedia)
