57e4970c3fa9f8e905e467679f4fd755de194ed9
samurai <navazesh.hossein@gmail.com>
2021-08-13 15:10:17 -0400

    contents(urls): initialize contents app

--- contents/urls/__init__.py
+++ contents/urls/__init__.py
@@ -1,6 +1,7 @@
 
 from .category import urlpatterns as category_urls
 from .multimedia import urlpatterns as multimedia_urls
+from .content import urlpatterns as contents_urls
 
 
-urlpatterns = category_urls+multimedia_urls
+urlpatterns = category_urls+multimedia_urls+contents_urls
