2a329facd001aed1e7e5ad3d8667ba254601cfa5
samurai <navazesh.hossein@gmail.com>
2021-08-17 10:01:55 -0400

    contents(urls): rename contents

--- contents/urls/content.py
+++ contents/urls/content.py
@@ -2,5 +2,5 @@ from rest_framework.routers import DefaultRouter
 from ..views import ContentViewSet
 
 router = DefaultRouter()
-router.register(r'content', ContentViewSet, basename='content')
+router.register(r'contents', ContentViewSet, basename='content')
 urlpatterns = router.urls
