03ca2e9efcdca1e4600152d88807bcc5f155f59f
alimohress <alimohress@gmail.com>
2021-10-15 19:52:33 +0330

    add related_name to content model

--- contents/models/content.py
+++ contents/models/content.py
@@ -9,5 +9,6 @@ class Content(BaseModel):
     body = models.TextField()
     last_modified = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='catagory', on_delete=models.CASCADE)
     files = models.ManyToManyField(Multimedia)
