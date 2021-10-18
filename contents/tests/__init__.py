0a73c37c573903f07cf2201781be964214207059
alimohress <alimohress@gmail.com>
2021-10-19 01:19:02 +0330

    import all tests

--- contents/tests/__init__.py
+++ contents/tests/__init__.py
@@ -1,2 +1,3 @@
-from .test_multimedia import MultimediaTestCase
+from .test_multimedia import MultimediaTestCase, MultimediaAPITestCase
 from .test_categories import CategoryTestCase
+from .test_contents import ContentTestCase
